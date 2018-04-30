5f83d8bab1ec7c7dc6456ef270978f28bc35fe44
chirag.p <chirag.p@loginextsolutions.com>
2018-05-01 00:52:17 +0530

    Commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,12 +16,19 @@
 
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class Main {
 
+	public static void main (String str[]){
+		SpringApplication.run(Main.class, str);
+	}
+	
 	  @RequestMapping("/")
 	  public String index() {
 	    return "hello";
